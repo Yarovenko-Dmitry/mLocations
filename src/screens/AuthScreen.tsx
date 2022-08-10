178ee2187d4864a3c6a861ed7fb5e0b081077a2c
Yarovenko Dmitry <yarovenkodmv@gmail.com>
2022-08-10 14:10:46 +0300

    feat: add Firebase & Google login for iOS

--- src/screens/AuthScreen.tsx
+++ src/screens/AuthScreen.tsx
@@ -8,6 +8,7 @@ import {AuthScreenType, SetIsLoggedType} from "./types";
 GoogleSignin.configure({
     // webClientId is in the google-services.json file, in the "client_id" field, above "client_type": 3 (!!!)
     webClientId: '486133882145-r8ahvo21ec07f5f4ud6vmbr5l37dirv8.apps.googleusercontent.com',
+    iosClientId: '486133882145-jtu2bhk6q5dme6a1oaclb0kri1q5qraf.apps.googleusercontent.com',
 });
 
 async function onGoogleButtonPress(setIsLogged: SetIsLoggedType) {
